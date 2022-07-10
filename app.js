306b4ee82201ec0ec30d0bb1f462dbef27e9bb43
Stimpy <masquare1009@gmail.com>
2022-07-10 17:01:22 +0200

    Added error handeling to app

--- app.js
+++ app.js
@@ -1,2 +1,23 @@
 // Simple weather app using free Weatherapi
-const request = require('node-fetch');
+const fetch = require('node-fetch');
+const url = 'http://api.weatherapi.com/v1/current.json?key=cc44c3e2a3524cdfa2f123819222705&q=London&aqi=no';
+
+const getWeatherData = async () => {
+    try {
+        let response = await fetch(url)
+
+        if (response.status === 200) {
+            return await response.json()
+        } else {
+            throw new Error('Something bad happened :(')
+        }
+    } catch (error) { console.log('Connection failed') }
+};
+
+getWeatherData()
+    .then((res) => {
+        console.log(res)
+    })
+    .catch((error) => {
+        console.log(error.message)
+    });
